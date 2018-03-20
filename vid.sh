7519678989eaf7470d283cd89f1d04ad23f5e44c
Io Mihai <iomihai@hotmail.com>
2018-03-20 18:43:03 +0100

    cu muzica

--- vid.sh
+++ vid.sh
@@ -4,6 +4,7 @@
 #for i in $(ls *.jpg); do exiv2 -r '%Y-%m-%d_%H-%M-%S.:basename:' rename $i; done
 
 OUT_FOLDER=/run/media/iomihai/dashcam2/timelapse/face_lapse
+MUZICA_FILE=/run/media/iomihai/dashcam2/dash_compus/concat/muzica_random.txt
 
 rsync -av --ignore-existing /mnt/nfs/diverse/camera/mihai/ $OUT_FOLDER/input_large
 for file in $OUT_FOLDER/input_large/*.jpg; do
@@ -16,8 +17,8 @@ done
 source ./venv/bin/activate
 ./pada.py align
 deactivate
-ffmpeg -y -threads 6 -loglevel error -framerate 15/1 -i $OUT_FOLDER/aligned/%08d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_15.mp4
-ffmpeg -y -threads 6 -loglevel error -framerate 10/1 -i $OUT_FOLDER/aligned/%08d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_10.mp4
-ffmpeg -y -threads 6 -loglevel error -framerate 5/1 -i $OUT_FOLDER/aligned/%08d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_5.mp4
+ffmpeg -y -threads 6 -loglevel error -framerate 15/1 -i $OUT_FOLDER/aligned/%08d.jpg -f concat -safe 0 -i $MUZICA_FILE -shortest -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_15.mp4
+ffmpeg -y -threads 6 -loglevel error -framerate 10/1 -i $OUT_FOLDER/aligned/%08d.jpg -f concat -safe 0 -i $MUZICA_FILE -shortest -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_10.mp4
+ffmpeg -y -threads 6 -loglevel error -framerate 5/1 -i $OUT_FOLDER/aligned/%08d.jpg -f concat -safe 0 -i $MUZICA_FILE -shortest -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_5.mp4
 printf "file '%s'\n" $OUT_FOLDER/input/*.jpg > $OUT_FOLDER/input.txt
-ffmpeg -y -threads 6 -loglevel error -f concat -safe 0 -i $OUT_FOLDER/input.txt -r 15/1 -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_input.mp4
+ffmpeg -y -threads 6 -loglevel error -f concat -safe 0 -i $OUT_FOLDER/input.txt -f concat -safe 0 -i $MUZICA_FILE -shortest -r 15/1 -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -movflags +faststart -r 30 $OUT_FOLDER/out/face_lapse_input.mp4
